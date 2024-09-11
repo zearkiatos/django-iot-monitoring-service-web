c07c99af9b6ae13d243541b08600a8c46c41ccd9
zearkiatos <caprilespe@outlook.com>
2024-09-10 22:39:22 -0300

    Updating application and update dependencies

--- IOTMonitoringServer/config/config.py
+++ IOTMonitoringServer/config/config.py
@@ -9,3 +9,14 @@ if environment != "local":
 class Config:
     ENVIRONMENT = environment
     APP_NAME=os.getenv('APP_NAME')
+    ALLOWED_HOSTS=os.getenv('ALLOWED_HOSTS')
+    DATABASE_HOST=os.getenv('DATABASE_HOST')
+    DATABASE_USER=os.getenv('DATABASE_USER')
+    DATABASE_PASSWORD=os.getenv('DATABASE_PASSWORD')
+    DATABASE_PORT=os.getenv('DATABASE_PORT')
+    DATABASE_NAME=os.getenv('DATABASE_NAME')
+    MQTT_HOST=os.getenv('MQTT_HOST')
+    MQTT_USER=os.getenv('MQTT_USER')
+    MQTT_PASSWORD=os.getenv('MQTT_PASSWORD')
+    MQTT_USER_PUB=os.getenv('MQTT_USER_PUB')
+    MQTT_PASSWORD_PUB=os.getenv('MQTT_PASSWORD_PUB')
--- makefile
+++ makefile
@@ -1,2 +1,18 @@
+activate:
+	if [ -d "venv" ]; then \
+		echo "Python 🐍 environment was activated"; \
+		source venv/bin/activate; \
+	else \
+		 echo "The folder environment doesn't exist"; \
+        python -m venv venv; \
+        source venv/bin/activate; \
+		 echo "The environment folder was created and the python 🐍 environment was activated"; \
+	fi
 install:
 	pip install -r requirements.txt --break-system-packages
+
+migrate:
+	python3 manage.py migrate
+
+start_mqtt:
+	 nohup python3 IOTMonitoringServer/manage.py start_mqtt &
