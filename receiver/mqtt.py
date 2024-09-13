f3119a704c1cb3c601fe92a6ca09b081791cedb5
zearkiatos <caprilespe@outlook.com>
2024-09-12 21:34:31 -0300

    Fixing receiver code

--- receiver/mqtt.py
+++ receiver/mqtt.py
@@ -4,6 +4,7 @@ import json
 import os
 import ssl
 import paho.mqtt.client as mqtt
+import uuid
 from django.conf import settings
 
 
@@ -48,7 +49,11 @@ def on_message(client: mqtt.Client, userdata, message: mqtt.MQTTMessage):
 
 def on_connect(client, userdata, flags, rc):
     print("Suscribiendo al tópico: " + settings.TOPIC)
-    client.subscribe(settings.TOPIC)
+    if rc == 0:
+        print("Connected successfully")
+        client.subscribe(settings.TOPIC)
+    else:
+        print(f"Failed to connect, return code {rc}")
     print("Servicio de recepcion de datos iniciado")
 
 
@@ -57,14 +62,18 @@ def on_disconnect(client: mqtt.Client, userdata, rc):
     Función que se ejecuta cuando se desconecta del broker.
     Intenta reconectar al bróker.
     '''
-    print("Desconectado con mensaje:" + str(mqtt.connack_string(rc)))
-    print("Reconectando...")
-    client.reconnect()
+    if rc != 0:
+        print("Desconectado con mensaje:" + str(mqtt.connack_string(rc)))
+        print(f"Unexpected disconnection. Return code: {rc}")
+        print("Reconectando...")
+        client.reconnect()
+    else:
+        print("Client disconnected normally")
 
 
 print("Iniciando cliente MQTT...", settings.MQTT_HOST, settings.MQTT_PORT)
 try:
-    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1, settings.MQTT_USER)
+    client = mqtt.Client(client_id=f"receiver_{uuid.uuid4()}")
     client.on_connect = on_connect
     client.on_message = on_message
     client.on_disconnect = on_disconnect
